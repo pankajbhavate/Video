dc5b764534ef857691f20ad710680a24bb624a4d
pankaj <psbhavate9130@gmail.com>
2025-02-01 17:54:36 +0530

    change

--- src/Test/Test2.java
+++ src/Test/Test2.java
@@ -5,7 +5,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("my name is test2");
+		System.out.println("my  name is test2");
 
 	}
 
